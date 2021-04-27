27756fa74cb25d6d5ee2e3646642d0dc438781ee
Neeraj Aggarwal <nea344@g.harvard.edu>
2021-04-26 21:10:37 -0400

    clean up and comments added

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # simple_app
 Simple Node.js App to demo session management using passport and fb authentication
+
+Purpose:  Create a simple app to prove out session management using facebook login strategy.
+
+Code: Landing page for the app checks for the session. Displays use name in case user is logged in.
+Otherwise, gives an option to login using facebook credentials.
+
+Steps:
+To be able to use this code, need to create an app at https://developers.facebook.com/
+and setup .env file for the project:
+FB_CLIENT_ID=$YOURAPPID$
+FB_CLIENT_SECRET="YOURAPPSECRET"
+FB_CALLBACK_URL=http://localhost:3000/auth/facebook/callback
+
+Callback URL will work for the testing purpose.
+
+Challenges:
+Had multiple issues while creating this simple app and learnt a lot
+- Couldn not get it working on repl due to the callback URL
+- Facebook was compialing about not using the secure connection.
+
+References:
+- http://www.passportjs.org/packages/passport-facebook/
+- https://www.twilio.com/blog/facebook-oauth-login-node-js-app-passport-js
+- Module on session management
--- routes/routes.js
+++ routes/routes.js
@@ -4,31 +4,31 @@ var router = express.Router();
 
 const auth = require('./auth');
 
+// Authentication is optional for the home page
+// in case user is available, display it
 router.get('/', auth.optional, function (req, res) {
-  console.log("user = ", req.user);
+  
   res.render('home', {
-    user: req.user // get the user out of session and pass to template
+    user: req.user 
   });
 });
 
-router.get('/error', auth.optional, function (req, res) {
-  res.render('home');
-});
 
+// Entry point when user enters facebook credentials.
 router.get('/auth/facebook', passport.authenticate('facebook', {
+
   scope: ['public_profile', 'email']
 }));
 
-router.get('/auth/facebook/callback',
-  passport.authenticate('facebook', {
+// callback url after the user is authenticated
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
     successRedirect: '/',
     failureRedirect: '/error'
-  }));
+}));
 
-router.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
+// in case there are anhy errors
+router.get('/error', auth.optional, function (req, res) {
+  res.render('home');
 });
-
 
 module.exports = router;
