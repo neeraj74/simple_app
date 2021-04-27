14e5a36903cf5d8acc445ccd57e65699e94296ce
Neeraj Aggarwal <nea344@g.harvard.edu>
2021-04-26 21:13:51 -0400

    clean up and comments added

--- controllers/passport.js
+++ controllers/passport.js
@@ -2,6 +2,35 @@ const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 require('dotenv').config();
 
+/* 
+   Notes
+*/
+
+// Simplest form of using FacebookStrategy
+// Pleas check other options on this strategy
+passport.use(new FacebookStrategy({
+
+    // appId created on facebook site
+    clientID: process.env.FB_CLIENT_ID,
+
+    // secret for the app
+    clientSecret: process.env.FB_CLIENT_SECRET,
+
+    // callback url
+    callbackURL: process.env.FB_CALLBACK_URL,
+
+    // facebook has a lot of information about the user
+    // we can indicate the fields needed here
+    profileFields: ["email", "name", "displayName"]
+
+}, function (accessToken, refreshToken, profile, done) {
+    
+    // from profile._json will have all the fields requested
+    // This is the place to extract them and save them locally.
+    return done(null, profile);
+    }
+));
+
 passport.serializeUser(function (user, cb){
     cb(null, user);
 });
@@ -9,14 +38,3 @@ passport.serializeUser(function (user, cb){
 passport.deserializeUser(function (obj, cb) {
     cb(null, obj);
 });
-
-
-passport.use(new FacebookStrategy({
-    clientID: process.env.FB_CLIENT_ID,
-    clientSecret: process.env.FB_CLIENT_SECRET,
-    callbackURL: process.env.FB_CALLBACK_URL,
-    profileFields: ["email", "name", "displayName"]
-}, function (accessToken, refreshToken, profile, done) {
-    return done(null, profile);
-    }
-));
